6d19602eb056ca8b1b459bdbcd2f9871e1f8d5b1
Dhaesha M <dhaesha1@gmail.com>
2022-05-17 18:08:04 -0400

    ssetting up NFTMint contract to inheret openzeppelin erc721 + ownable contracts

---
+++ contracts/NFTMint.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity >=0.4.22 <0.9.0;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+
+// inheriting imported contracts  
+contract NFTMint is ERC721, Ownable {
+
+}
